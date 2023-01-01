19624b76e383212a1ca511cc8e2ea685f1ae0748
Daniel <41345110+zazu7765@users.noreply.github.com>
2023-01-01 02:45:29 -0500

    fraction addition :)

--- src/main/java/org/zsazu/Fraction.java
+++ src/main/java/org/zsazu/Fraction.java
@@ -1,4 +1,32 @@
 package org.zsazu;
 
 public class Fraction {
+
+    private final int numerator;
+    private final int denominator;
+    Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+    public String toString(){
+        return numerator+"/"+denominator;
+    }
+
+    public int numerator(){
+        return numerator;
+    }
+    public int denominator(){
+        return denominator;
+    }
+    private int gcd(int a, int b) { return b==0 ? a : gcd(b, a%b); }
+    public Fraction add(Fraction other){
+        // calculate numerator by multiplying the other's denominator
+        int newNumerator = this.numerator*other.denominator + other.numerator*this.denominator;
+        // denominators are multiplied
+        int newDenominator = this.denominator * other.denominator;
+        // reduce fraction to it's lowest possible form
+        int factor = gcd(newNumerator, newDenominator);
+        // return new fraction
+        return new Fraction(newNumerator/factor, newDenominator/factor);
+    }
 }
--- src/main/java/org/zsazu/Main.java
+++ src/main/java/org/zsazu/Main.java
@@ -3,6 +3,11 @@ package org.zsazu;
 public class Main {
     public static void main(String[] args) {
         SimpleCalculator calculator = new SimpleCalculator();
-        System.out.println(calculator.add(1,2));
+        Fraction half = new Fraction(1,2);
+        Fraction third = new Fraction(1,3);
+        System.out.println(half.add(third));
+        System.out.println(calculator.add(5,7));
+        System.out.println(calculator.subtract(45,11));
+        System.out.println(calculator.multiply(half.denominator(),third.numerator()));
     }
 }
