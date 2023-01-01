a204b24136efee92b1dbe5473c2abca5bca1865c
Daniel <41345110+zazu7765@users.noreply.github.com>
2022-12-31 22:32:47 -0500

    add, subtract, multiply, divide, modulo methods

--- src/main/java/org/zsazu/Main.java
+++ src/main/java/org/zsazu/Main.java
@@ -2,6 +2,7 @@ package org.zsazu;
 
 public class Main {
     public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
         System.out.println("Hello world!");
     }
 }
--- src/main/java/org/zsazu/SimpleCalculator.java
+++ src/main/java/org/zsazu/SimpleCalculator.java
@@ -1,7 +1,20 @@
 package org.zsazu;
 
 public class SimpleCalculator {
-    SimpleCalculator(){
-
+    SimpleCalculator(){}
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int subtract(int a, int b){
+        return a-b;
+    }
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public double divide(int a, int b){
+        return a/b;
+    }
+    public int modulo(int a, int b){
+        return a%b;
     }
 }
