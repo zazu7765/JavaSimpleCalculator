c7fc1f362c9de63813818e120fb0f1c484242eec
Daniel <41345110+zazu7765@users.noreply.github.com>
2023-01-01 03:36:41 -0500

    constructor made, refactor later

--- src/main/java/org/zsazu/CarLoanCalculator.java
+++ src/main/java/org/zsazu/CarLoanCalculator.java
@@ -1,4 +1,21 @@
 package org.zsazu;
 
 public class CarLoanCalculator {
+    CarLoanCalculator(int loanAmount, int loanLength, int loanRate, int downPayment) throws Exception {
+        if(loanLength<=0 || loanRate <=0){
+            throw new Exception("Length of loan is too short or interest rate is too low!");
+        }
+        else if(downPayment >= loanAmount){
+            throw new Exception("Your down payment is enough to cover the cost of the car!");
+        }
+        else{
+            int remainingBalance = loanAmount - downPayment;
+            int months = loanLength*12;
+            int monthlyBalance = remainingBalance/months;
+            int interest = monthlyBalance*loanRate/100;
+            int monthlyPayment = monthlyBalance+interest;
+            System.out.println(monthlyPayment);
+
+        }
+    }
 }
--- src/main/java/org/zsazu/Main.java
+++ src/main/java/org/zsazu/Main.java
@@ -1,8 +1,14 @@
 package org.zsazu;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SimpleCalculator calculator = new SimpleCalculator();
+        try {
+            CarLoanCalculator car = new CarLoanCalculator(10000, 3, 5, 2000);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
         Fraction half = new Fraction(1,2);
         Fraction third = new Fraction(1,3);
         System.out.println(half.add(third));
